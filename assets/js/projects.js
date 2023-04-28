fbecf3ea4657d17bb8e2ce2db1543c0f7ac179f2
Gabriel Camurça <gabriel@brtech.dev>
2023-04-28 15:23:17 -0300

    Edit image project-1

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,7 +1,7 @@
 export default [
   {
     text: 'Personotes: Aplicativo de anotações pessoais utilizando Laravel.',
-    image: 'project-1.png',
+    image: 'project-1.jpg',
   },
   {
     text: 'Personotes: Aplicativo de anotações pessoais utilizando Laravel.',
