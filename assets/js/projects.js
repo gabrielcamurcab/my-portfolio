f5d97213197e9de51e00c1609f091bf52d5c3e05
gabrielbrtech <gabrielcamurca@brtech.dev>
2023-04-29 10:37:42 -0300

    Add site for Personotes

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,6 +2,7 @@ export default [
   {
     text: 'Personotes: Aplicativo de anotações pessoais utilizando Laravel.',
     image: 'project-1.jpg',
+    url: 'https://github.com/gabrielcamurcab/personotes'
   },
   {
     text: 'Personotes: Aplicativo de anotações pessoais utilizando Laravel.',
